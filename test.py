8f33d159dbf6a59ffd35805a73cea7f1e8371b52
BrendaWuS <122028838+BrendaWuS@users.noreply.github.com>
2023-06-15 16:38:42 +0800

    Update file

--- test.py
+++ test.py
@@ -1,8 +1,64 @@
+# from github import Github
+# import os
+#
+# # 所有权限令牌token
+# token = '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'
+# # 创建Github对象
+# g = Github(token)
+#
+# # 获取要操作的仓库
+# owner = 'BrendaWuS'
+# repo = 'test4'
+# repository = g.get_repo(f"{owner}/{repo}")
+#
+# # 项目目录路的根路径
+# root_directory = os.path.dirname(__file__)  # E:\pythonProject\GithubTest3
+#
+#
+#
+# def add_directory(directory_path):
+#     for root, dirs, files in os.walk(directory_path):
+#         for file in files:
+#             file_path = os.path.join(root, file)
+#             with open(file_path, 'r', encoding='utf-8') as f:
+#                 content = f.read()
+#                 # 计算文件相对于项目根路径的相对路径
+#                 relative_path = os.path.relpath(file_path, directory_path)
+#                 # 在指定路径下创建文件并添加内容
+#                 repository.create_file(relative_path, "Add file", content, branch=repository.default_branch)
+#
+#
+# def commit_changes():
+#     # 获取默认分支的引用
+#     ref = repository.get_git_ref(f"heads/{repository.default_branch}")
+#
+#     # 获取最新提交
+#     latest_commit = repository.get_commit(ref.object.sha)
+#
+#     # 递归地添加项目目录下的所有文件
+#     add_directory(root_directory)
+#
+#     # 创建新的提交
+#     repository.create_git_commit("Commit message", latest_commit.commit.tree, [latest_commit])
+#
+#
+# def push_changes():
+#     # 强制更新引用，以便推送更改
+#     ref = repository.get_git_ref(f"heads/{repository.default_branch}")
+#     ref.edit(ref.object.sha, force=True)
+#
+#
+# # 调用相关函数进行操作
+# commit_changes()
+# push_changes()
+
+
+
 from github import Github
 import os
 
 # 所有权限令牌token
-token = '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'
+token = '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'
 # 创建Github对象
 g = Github(token)
 
@@ -12,7 +68,7 @@ repo = 'test4'
 repository = g.get_repo(f"{owner}/{repo}")
 
 # 项目目录路的根路径
-root_directory = os.path.dirname(__file__)  # E:\pythonProject\GithubTest3
+root_directory = os.path.dirname(os.path.abspath(__file__))
 
 
 # 添加文件、提交更改和推送到远程仓库的函数
@@ -24,19 +80,25 @@ def add_directory(directory_path):
                 content = f.read()
                 # 计算文件相对于项目根路径的相对路径
                 relative_path = os.path.relpath(file_path, directory_path)
-                # 在指定路径下创建文件并添加内容
-                repository.create_file(relative_path, "Add file", content)
+
+                try:
+                    # 尝试创建文件
+                    repository.create_file(relative_path, "Add file", content)
+                except Exception:
+                    # 文件已存在，尝试更新文件
+                    file = repository.get_contents(relative_path)
+                    repository.update_file(file.path, "Update file", content, file.sha)
 
 
 def commit_changes():
+    # 递归地添加项目目录下的所有文件
+    add_directory(root_directory)
+
     # 获取默认分支的引用
     ref = repository.get_git_ref(f"heads/{repository.default_branch}")
 
     # 获取最新提交
     latest_commit = repository.get_commit(ref.object.sha)
-
-    # 递归地添加项目目录下的所有文件
-    add_directory(root_directory)
 
     # 创建新的提交
     repository.create_git_commit("Commit message", latest_commit.commit.tree, [latest_commit])
@@ -51,3 +113,4 @@ def push_changes():
 # 调用相关函数进行操作
 commit_changes()
 push_changes()
+
